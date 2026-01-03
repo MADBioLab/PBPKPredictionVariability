ff9c3fbfbef9c5572b5851053a5b348996542ea8
bsakpa <133530282+bsakpa@users.noreply.github.com>
2026-01-03 01:46:01 -0500

    Update MonteCarloSimulations.m

--- MonteCarloSimulations.m
+++ MonteCarloSimulations.m
@@ -5,7 +5,7 @@ function MonteCarloSimulations(referenceProperties, selectedTissue, Nsamples, er
     %                       interest
     % selectedTissue - numerical ID of tissue of interest
     % Nsamples - number of Monte Carlo realizations to generate
-    %errorScale - scaling factor for mean average errors in parameter distributions
+    % errorScale - scaling factor for mean average errors in parameter distributions
 
     %% specify drug dose, infusion duration, and simulation time
     dose = 15; %[mg/kg]
@@ -18,8 +18,8 @@ function MonteCarloSimulations(referenceProperties, selectedTissue, Nsamples, er
     Nmodels = 4; %number of Kpu models to be evaluated
     
     %% evaluate Monte Carlo outcomes for each molecule
-    MCResults=zeros(Nsamples,Noutputs*Nmodels,Nmolecules);
-    nominalResults=zeros(Noutputs*Nmodels,Nmolecules);
+    MCResults = zeros(Nsamples,Noutputs*Nmodels,Nmolecules);
+    nominalResults = zeros(Noutputs*Nmodels,Nmolecules);
 
     for j=1:Nmolecules
 
@@ -58,12 +58,12 @@ function [MCoutcomes,nominalOutcomes] = sampleAndSimulate(...
     %% Type of distribution to sample for each property
     % order: RMM pKa_a pKa_b fu logCLint logD 
     % Beta or Normal 
-    sampleType={'None',...
-                'Beta',...
-                'Beta',...
-                'Beta',...
-                'Norm',...
-                'Norm'};
+    sampleType = {'None',...
+                  'Beta',...
+                  'Beta',...
+                  'Beta',...
+                  'Norm',...
+                  'Norm'};
 
     %% Upper and lower beta distribution limits [flag as =1 if normal or lognormal]
     maxmin = [[1,14,14,1,   1, 1];...
@@ -80,14 +80,14 @@ function [MCoutcomes,nominalOutcomes] = sampleAndSimulate(...
     %% Sample properties from distributions
     distributedParams = zeros(Nsamples,Nparams);
     distributedParams(:,1) = propertyInputs(1); %RMM is fixed
-    for i=2:6 
+    for i = 2:Nparams
         distributedParams(:,i) = sampleDistribution(propertyInputs(i),...
                                  MAE(i),sampleType{i},Nsamples,maxmin(:,i));
     end
     
     %% convert logCLint to CLint
-    distributedParams(:,5)=10.^distributedParams(:,5); 
-    propertyInputs(5)=10^propertyInputs(5);
+    distributedParams(:,5) = 10.^distributedParams(:,5); 
+    propertyInputs(5) = 10^propertyInputs(5);
 
     %%  define complete property arrays
     allProperties = [propertyInputs;distributedParams];
@@ -128,39 +128,39 @@ function [MCoutcomes,nominalOutcomes] = sampleAndSimulate(...
         P.KpuFunc = @PearceNoCorrectionsKpuModel;
         P4 = SimulatePBPK(P,Tmax);
 
-        Vdeq(i,:)=[P1.Vdeq,P2.Vdeq,P3.Vdeq,P4.Vdeq];
-        AUCu(i,:)=secs2mins([P1.AUCu(selectedTissue),...
-                             P2.AUCu(selectedTissue),...
-                             P3.AUCu(selectedTissue),...
-                             P4.AUCu(selectedTissue),]); %uM.min
-        Cmaxu(i,:)=[P1.Cmaxu(selectedTissue),...
-                    P2.Cmaxu(selectedTissue),...
-                    P3.Cmaxu(selectedTissue),...
-                    P4.Cmaxu(selectedTissue)]; %uM
-        T1uM(i,:)=[P1.T1uM(selectedTissue),...
-                   P2.T1uM(selectedTissue),...
-                   P3.T1uM(selectedTissue),...
-                   P4.T1uM(selectedTissue)]; %hrs    
+        Vdeq(i,:) = [P1.Vdeq,P2.Vdeq,P3.Vdeq,P4.Vdeq];
+        AUCu(i,:) = secs2mins([P1.AUCu(selectedTissue),...
+                               P2.AUCu(selectedTissue),...
+                               P3.AUCu(selectedTissue),...
+                               P4.AUCu(selectedTissue),]); %uM.min
+        Cmaxu(i,:) = [P1.Cmaxu(selectedTissue),...
+                      P2.Cmaxu(selectedTissue),...
+                      P3.Cmaxu(selectedTissue),...
+                      P4.Cmaxu(selectedTissue)]; %uM
+        T1uM(i,:) = [P1.T1uM(selectedTissue),...
+                     P2.T1uM(selectedTissue),...
+                     P3.T1uM(selectedTissue),...
+                     P4.T1uM(selectedTissue)]; %hrs    
 
     end
     
     %% assemble output for all Monte Carlo realizations, all models, and all outcomes 
-    MCoutcomes=[Vdeq,Cmaxu,AUCu,T1uM];
-    nominalOutcomes=MCoutcomes(1,:);
-    MCoutcomes=MCoutcomes(2:end,:);
+    MCoutcomes = [Vdeq,Cmaxu,AUCu,T1uM];
+    nominalOutcomes = MCoutcomes(1,:);
+    MCoutcomes = MCoutcomes(2:end,:);
 
 end
 
 %%
 
 function t = secs2mins(x)
-    t=x/60;
+    t = x/60;
 end
 
 %%
 
 function t = mins2secs(x)
-    t=x*60;
+    t = x*60;
 end
 
 %%
@@ -168,17 +168,17 @@ end
 function distributedParams = sampleDistribution(theta,MAE,sampleType,Nsamples,maxmin)
     if strcmp(sampleType,'Norm')
 
-        distributedParams=generate_dist(theta,MAE,sampleType,Nsamples,maxmin);    
+        distributedParams = generate_dist(theta,MAE,sampleType,Nsamples,maxmin);    
 
     elseif strcmp(sampleType,'LogN')
 
-        mu = log((theta^2)/sqrt(MAE^2+theta^2));
-        sigma = sqrt(log(MAE^2/(theta^2)+1));        
+        mu = log((theta^2) / sqrt(MAE^2+theta^2));
+        sigma = sqrt(log(MAE^2 / (theta^2) + 1));        
         distributedParams = generate_dist(mu,sigma,sampleType,Nsamples,maxmin);
 
     elseif strcmp(sampleType,'Beta')
 
-        distributedParams=generate_dist(theta,MAE,sampleType,Nsamples,maxmin);
+        distributedParams = generate_dist(theta,MAE,sampleType,Nsamples,maxmin);
 
     end
 end
@@ -190,22 +190,22 @@ function f=generate_dist(mu,MAE,sampleType,N,maxmin)
     if strcmp(sampleType,'Beta')
 
         % normalize mean and MAE (max-min normalization)
-        d = maxmin(1)-maxmin(2);
-        mu = (mu-maxmin(2))/d;
+        d = maxmin(1) - maxmin(2);
+        mu = (mu - maxmin(2))/d;
         MAE = MAE/d;
 
         % find beta distribution parameters that provide desired mean and
         % error
-        [a,b]=findBeta(mu,MAE);
+        [a,b] = findBeta(mu,MAE);
 
         % sample from parameterized beta distribution and scale to desired
         % domain
-        f=random(sampleType,a,b,[N,size(mu,2)]) * d + maxmin(2);
+        f = random(sampleType,a,b,[N,size(mu,2)]) * d + maxmin(2);
 
     else 
         
-        sigma=MAE/sqrt(2/pi);
-        f=random(sampleType,mu,sigma,[N,size(mu,2)]);
+        sigma = MAE/sqrt(2/pi);
+        f = random(sampleType,mu,sigma,[N,size(mu,2)]);
 
     end
 
@@ -223,7 +223,7 @@ function [a,b] = findBeta(mu,sigma)
     while test > 1e-3
 
         a = fminbnd(@(a,mu,sigma)assessMAEdiscrepancy(a,mu,sigma),0,1e6,[],mu,sigma);
-        b = a*(1-mu)/mu;
+        b = a * (1-mu) / mu;
         test = assessMAEdiscrepancy(a,mu,sigma);
         
         % if a & b estimates fail to satisfy desired MAE, nudge mean and try again
@@ -245,7 +245,7 @@ function f = assessMAEdiscrepancy(a,mu,MAE)
     %determine whether value of b estimated from a and distribution mean
     %provides the desired MAE for the distribution
 
-    b = a*(1-mu)/mu;
+    b = a * (1-mu) / mu;
     x = betarnd(a,b,[10000,1]);
     D = mean(abs(x-mu));
     f = abs(D-MAE);
